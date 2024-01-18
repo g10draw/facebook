3871cfe80d31243b9aa061e5bb8c001b01ebce9d
g10draw <mjitendrareddy@gmail.com>
2024-01-18 17:06:29 +0530

    implemented home_page and timeline_page views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .forms import PostForm
+from .models import Post
+
 # Create your views here.
+def home_page(request):
+    return render(request, 'blog/home-page.html')
+
+def timeline_page(request):
+    form = PostForm()
+    posts = Post.objects.all().order_by('-posted_at')
+    return render(request, 'blog/timeline.html', 
+        {'form': form, 'posts': posts})
