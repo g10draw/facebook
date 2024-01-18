a20398af2b2ebc09bf7f12746c344aa23bfeb75b
g10draw <mjitendrareddy@gmail.com>
2024-01-18 17:06:11 +0530

    added home and timeline urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,1 +1,9 @@
-urlpatterns=[]
+from django.urls import path
+
+from .views import timeline_page, home_page
+
+
+urlpatterns=[
+    path('', home_page, name='home-page'),
+    path('timeline', timeline_page, name='timeline-page'),
+]
