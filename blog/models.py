b2f5e383c699fb86c9d4c075578d36eac1d2d953
g10draw <mjitendrareddy@gmail.com>
2024-01-18 17:04:07 +0530

    modified likes to accept null values

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,12 @@ class Post(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     title = models.TextField()
     image = models.ImageField(upload_to='posts/images/', blank=True, null=True)
-    likes = models.ManyToManyField(CustomUser, related_name='my_liked_posts')
+    likes = models.ManyToManyField(CustomUser, related_name='my_liked_posts', 
+        null=True, blank=True)
     posted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
