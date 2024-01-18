b2580326da069b2c21a2e3b6c888a1e0a309dab9
g10draw <mjitendrareddy@gmail.com>
2024-01-18 15:40:01 +0530

    Comment model created

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,12 @@ class Post(models.Model):
     image = models.ImageField(upload_to='posts/images/', blank=True, null=True)
     likes = models.ManyToManyField(CustomUser, related_name='my_liked_posts')
     posted_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(CustomUser, related_name='my_comments', 
+        on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', 
+        on_delete=models.CASCADE)
+    text = models.TextField()
+    commented_at = models.DateTimeField(auto_now_add=True)
