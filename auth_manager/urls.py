3acf3480214e02e063499296f5fd3e48c3b10fa3
g10draw <mjitendrareddy@gmail.com>
2024-01-18 17:03:13 +0530

    added auth urls

--- auth_manager/urls.py
+++ auth_manager/urls.py
@@ -1,1 +1,9 @@
-urlpatterns=[]
+from django.urls import path
+
+from .views import login_user, register_user, logout_user
+
+urlpatterns=[
+    path('login', login_user, name='login-page'),
+    path('register_user', register_user, name='register-page'),
+    path('logout_user', logout_user, name='logout-page'),
+]
