fb8b64b3ea3c3788b17b53f91bbfd62e9dd3c8c0
g10draw <mjitendrareddy@gmail.com>
2024-01-19 15:42:40 +0530

    added create-post url

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import timeline_page, home_page
+from .views import timeline_page, home_page, create_post
 
 
 urlpatterns=[
     path('', home_page, name='home-page'),
     path('timeline', timeline_page, name='timeline-page'),
+    path('create-post', create_post, name='create-post'),
 ]
