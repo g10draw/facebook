0f607ae090a887f1f3cd6001f15d62e9ed3037f4
g10draw <mjitendrareddy@gmail.com>
2024-01-20 12:14:44 +0530

    added update-like path

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import timeline_page, home_page, create_post
+from .views import timeline_page, home_page, create_post, update_like
 
 
 urlpatterns=[
     path('', home_page, name='home-page'),
     path('timeline', timeline_page, name='timeline-page'),
     path('create-post', create_post, name='create-post'),
+    path('update-like', update_like, name='update-like'),
 ]
