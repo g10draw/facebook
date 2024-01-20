6e3464239cd50bafff28375cb5dce171c6bead00
g10draw <mjitendrareddy@gmail.com>
2024-01-20 12:14:08 +0530

    added PostAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import Post
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
