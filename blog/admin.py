4614d93d1945767ad914ac0388ddc727ee4715a1
g10draw <mjitendrareddy@gmail.com>
2024-01-22 17:16:59 +0530

    added Comment model

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'post', 'text')
+
 # Register your models here.
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
