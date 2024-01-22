41a33361d9b6c96e56cd67e23a49efb98984dc85
g10draw <mjitendrareddy@gmail.com>
2024-01-22 15:44:39 +0530

    added add-comment path

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import timeline_page, home_page, create_post, update_like
+from .views import (timeline_page, home_page, create_post, 
+    update_like, add_comment)
 
 
 urlpatterns=[
@@ -8,4 +9,5 @@ urlpatterns=[
     path('timeline', timeline_page, name='timeline-page'),
     path('create-post', create_post, name='create-post'),
     path('update-like', update_like, name='update-like'),
+    path('add-comment/<int:post_id>', add_comment, name='add-comment'),
 ]
