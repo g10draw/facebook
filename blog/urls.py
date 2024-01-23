8c346e5c5c4f139e0a9a24e6df73a621fbb7a2b3
g10draw <mjitendrareddy@gmail.com>
2024-01-23 16:59:49 +0530

    added paths for handle-follow

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import (timeline_page, home_page, create_post, 
-    update_like, add_comment, profile_page)
+    update_like, add_comment, profile_page, handle_follow)
 
 
 urlpatterns=[
@@ -9,6 +9,7 @@ urlpatterns=[
     path('timeline', timeline_page, name='timeline-page'),
     path('create-post', create_post, name='create-post'),
     path('update-like', update_like, name='update-like'),
+    path('handle-follow/<int:user_id>', handle_follow, name='handle-follow'),
     path('add-comment/<int:post_id>', add_comment, name='add-comment'),
-    path('profile-page', profile_page, name='profile-page'),
+    path('profile-page/<int:user_id>', profile_page, name='profile-page'),
 ]
